cc0406abc6e5baadcc523a887ecb2162445c2806
mbashas <sethmbasha@gmail.com>
2025-06-29 22:29:00 +0300

    Add edited MySQLServer.py file

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,25 @@
 import mysql.connector
 from mysql.connector import Error
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=""
-)
-
-if mydb.is_connected():
-    mycursor = mydb.cursor()
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=""
+    )
     
-    # Create database if it doesn't exist
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-    
-    print("Database 'alx_book_store' created successfully!")
-    
-    mycursor.close()
-    mydb.close()
-else:
-    print("Error connecting to MySQL")
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        
+        # Create database if it doesn't exist
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        
+        print("Database 'alx_book_store' created successfully!")
+        
+        mycursor.close()
+        mydb.close()
+    else:
+        print("Error connecting to MySQL")
+        
+except mysql.connector.Error as e:
+    print(f"Error connecting to MySQL: {e}")
