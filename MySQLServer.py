5db9904f882bafb324a079df0e108cd98c4e231d
mbashas <sethmbasha@gmail.com>
2025-06-29 22:28:08 +0300

    Add edited MySQLServer.py file

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import Error
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -6,12 +7,15 @@ mydb = mysql.connector.connect(
     password=""
 )
 
-mycursor = mydb.cursor()
-
-# Create database if it doesn't exist
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-
-print("Database 'alx_book_store' created successfully!")
-
-mycursor.close()
-mydb.close()
+if mydb.is_connected():
+    mycursor = mydb.cursor()
+    
+    # Create database if it doesn't exist
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    
+    print("Database 'alx_book_store' created successfully!")
+    
+    mycursor.close()
+    mydb.close()
+else:
+    print("Error connecting to MySQL")
